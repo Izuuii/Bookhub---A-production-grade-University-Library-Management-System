a216d9a2f720c61228a4406b755d93ea1c49a6c3
Izuuii <denverdeleon22@gmail.com>
2025-09-21 22:57:39 +0800

    Added Icons and Images files, and prettier file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { clsx } from 'clsx'; // Add this import
+import Image from 'next/image';
 
 const Header = () => {
-  return <div>Header</div>;
+  const pathname = usePathname();
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link
+            href="/library"
+            className={clsx(
+              // Use clsx instead of cn
+              'text-base cursor-pointer capitalize',
+              pathname === '/library' ? 'text-light-200' : 'text-light-100'
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
